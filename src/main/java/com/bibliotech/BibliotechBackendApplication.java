ef3e38b0383f442ad859b4a72448d13bef6c4297
Daniela Nogueira Rampim <daninogueira.dev@gmail.com>
2025-03-11 15:52:34 -0300

    🚚chore: organizing folders

--- src/main/java/com/bibliotech/BibliotechBackendApplication.java
+++ src/main/java/com/bibliotech/BibliotechBackendApplication.java
@@ -1,4 +1,4 @@
-package com.bibliotech.bibliotech_backend;
+package com.bibliotech;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
